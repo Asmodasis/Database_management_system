76a92c96d337356ce79213e323d036be99961ced
Shawn Ray <Asmodasis@gmail.com>
2022-09-22 17:40:25 -0700

    Initial commit
    Base upload to confirm github is working

---
+++ DBMS.py
@@ -0,0 +1,14 @@
+import os
+
+class DBMS:
+    # creates the database management system, this will form the link with the terminal to accept input
+    def __init__(self):
+        pass
+
+    def createDatabase(self):
+        pass
+    
+    def deleteDatabase(self):
+        pass
+
+    
---
+++ Table.py
@@ -0,0 +1,17 @@
+class Table:
+
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+    
+    def query(self):
+        pass
+        
